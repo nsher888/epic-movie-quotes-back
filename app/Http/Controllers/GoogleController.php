42ac6647e6a3b8331e6bed41d3bc8cdc06f4edee
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-06-29 20:10:55 +0400

    chore: remove comments

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -4,24 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Client\Response as ClientResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Response;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
-    public function redirect()
+    public function redirect(): RedirectResponse
     {
         return Socialite::driver('google')->stateless()->redirect();
     }
 
-    public function callback()
+    public function callback(): JsonResponse
     {
         $user = Socialite::driver('google')->user();
         return  response()->json($user);
     }
 
-    public function login()
+    public function login(): Response
     {
         $googleUser = Socialite::driver('google')->stateless()->user();
         $user = User::where('email', $googleUser->getEmail())->first();
--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -8,13 +8,14 @@ use App\Http\Requests\storeEmailRequest;
 use App\Http\Requests\storeResetPasswordRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
-    public function sendResetLink(storeEmailRequest $request)
+    public function sendResetLink(storeEmailRequest $request): JsonResponse
     {
         app()->setLocale($request->local);
         $request->validated();
@@ -27,12 +28,12 @@ class ResetPasswordController extends Controller
          : response()->json('email not found', 404);
     }
 
-    public function showResetForm($token)
+    public function showResetForm($token): JsonResponse
     {
         return response()->json(['token' => $token, 'email' => request()->email]);
     }
 
-    public function resetPassword(ResetRequest $request)
+    public function resetPassword(ResetRequest $request): JsonResponse
     {
         $request->validated();
         $status = Password::reset(
